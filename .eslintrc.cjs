2b2db7a1498a92badc779e8e874ed8d9bfb91b93
Otchy <id+github@otchy.net>
2022-04-05 11:39:41 +0900

    Introduce eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "es2022": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+    },
+    "plugins": [
+        "@typescript-eslint", "import", "unused-imports"
+    ],
+    "ignorePatterns": [],
+    "rules": {
+        "no-case-declarations": "off",
+        "prettier/prettier": ["error", {
+            "tabWidth": 4,
+            "singleQuote": true,
+            "printWidth": 160,
+        }],
+        "import/order": [
+            "error",
+            {
+                "groups": ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+                "newlines-between": "never",
+                "alphabetize": { "order": "asc", "caseInsensitive": true },
+            }
+        ],
+        "unused-imports/no-unused-imports": "error"
+    }
+};
